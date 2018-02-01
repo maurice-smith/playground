b40beda91faed7e4b34b2448aa4c1a94c4e84012
Maurice Smith <mo.maurice@gmail.com>
2018-02-01 00:07:20 -0500

    adding data binding

--- android/examples/RoomEx/app/src/main/java/com/kingmo/roomex/MainActivity.java
+++ android/examples/RoomEx/app/src/main/java/com/kingmo/roomex/MainActivity.java
@@ -3,11 +3,29 @@ package com.kingmo.roomex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.databinding.DataBindingUtil;
+import android.view.View;
+import android.widget.Toast;
+
+import com.kingmo.roomex.databinding.ActivityMainBinding;
+import com.kingmo.roomex.viewmodel.RosterEntryViewModel;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setViewModel(new RosterEntryViewModel());
+
+
+        binding.submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hey There!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ android/examples/RoomEx/app/src/main/java/com/kingmo/roomex/viewmodel/RosterEntryViewModel.java
@@ -0,0 +1,8 @@
+package com.kingmo.roomex.viewmodel;
+
+/**
+ * Created by kingmo on 1/31/18.
+ */
+
+public class RosterEntryViewModel {
+}
