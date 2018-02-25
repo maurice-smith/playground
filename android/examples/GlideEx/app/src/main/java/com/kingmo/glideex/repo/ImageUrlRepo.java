c7503eb4e05a121e7b26fbb7c2daf0bdfe2d9c30
Maurice Smith <mo.maurice@gmail.com>
2018-02-25 17:19:23 -0500

    Glide Ex: showing placeholder

--- android/examples/GlideEx/app/src/main/java/com/kingmo/glideex/repo/ImageUrlRepo.java
+++ android/examples/GlideEx/app/src/main/java/com/kingmo/glideex/repo/ImageUrlRepo.java
@@ -15,6 +15,7 @@ public class ImageUrlRepo {
         IMGAE_LIST.add("https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/H-P/lion-pride.adapt.945.1.jpg");
         IMGAE_LIST.add("https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/H-P/lion-cub-yawning.adapt.945.1.jpg");
         IMGAE_LIST.add("https://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Wild%20Cats/ww-wild-cats-lion.adapt.945.1.jpg");
+        IMGAE_LIST.add(null);
     }
 
     public List<String> getImageList() {
