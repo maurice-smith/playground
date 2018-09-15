c9121a6f6f7c0763bbd09d8a9ad8f84d83c6d0e7
Maurice Smith <mo.maurice@gmail.com>
2018-09-15 09:18:29 -0400

    adding Post Dao access methods

--- android/archComponents/Pager/app/src/main/java/com/kingmo/pager/database/PostsDao.java
+++ android/archComponents/Pager/app/src/main/java/com/kingmo/pager/database/PostsDao.java
@@ -2,15 +2,29 @@ package com.kingmo.pager.database;
 
 import com.kingmo.pager.database.entity.Post;
 
+import java.util.List;
+
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import io.reactivex.Flowable;
 
 @Dao
 public interface PostsDao {
-    @Insert
-    void add(Post... mates);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void add(Post... posts);
 
     @Delete
-    void delete(Post... mates);
+    void delete(Post... posts);
+
+    @Query("DELETE FROM posts")
+    void deleteAll();
+
+    @Query("SELECT * FROM posts LIMIT x = :numToReturn,y = :offset")
+    Flowable<List<Post>> getPosts(int numToReturn, int offset);
+
+    @Query("SELECT * FROM post WHERE id = :postId LIMIT 1")
+    Flowable<Post> getPost(int postId);
 }
